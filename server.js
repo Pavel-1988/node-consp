e23ac879a7458058ba82f2160fba6a5def6d38a1
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2023-02-12 15:38:50 +0200

    end les

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan')
 const PORT = 8081;
 
-app.get('/home', (req, res) => {
-  res.send('get request');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use(morgan('tiny'))
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}, ${new Date().toISOString()}`)
+  next();
 });
 
-app.post('/home', (req, res) => {
-  res.send('post request');
-});
-
-app.delete('/home', (req, res) => {
-  res.send('gelete request');
-});
-
-app.use((req, res) => {
-  res.send('midlwere request');
+app.post('/home', (req, res,) => {
+  if (!req.body.goit) {
+    res.status(400).json({status: 'goit parameter is required'})
+  }
+  res.json({javascript: 'object', body: req.body});
 });
 
 
