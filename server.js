b9b406d7c1e93572a5dd9b90d5f0053cb271ff14
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2023-02-15 23:15:25 +0200

    wth

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const morgan = require('morgan')
+const {router} = require('./booksRouter')
 const PORT = 8081;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
+app.use('/api',router);
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.originalUrl}, ${new Date().toISOString()}`)
@@ -27,3 +29,5 @@ app.listen(PORT, (err) => {
   }
   console.log(`Server work ${PORT}`)
 });
+
+// погода АПИ ключ - ea41bc3d393b41638cdd827b88acf3ef
